94f1e7fd88779aebd6350840392afc4bef95d86b
Yakov <106259638+YakovSalman@users.noreply.github.com>
2023-08-30 13:09:29 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -598,7 +598,12 @@ function choiceDays () {
         const currentYears = currentWrap.querySelector('.calendar-year');
         // const dateFirst = new Date(`${parseInt(variableName.textContent)}, ${parseInt(currentYears.textContent)}, ${currentMonth.textContent}`);
 
-        const dateFirst = new Date(`${parseInt(currentYears.textContent)}-${currentMonth.textContent}-${parseInt(variableName.textContent)}`);
+        const year = parseInt(currentYears.textContent);
+        const month = currentMonth.textContent;
+        const day = parseInt(variableName.textContent);
+
+
+        const dateFirst = new Date(`${year}-${month}-${day}`);
 
         // const temporary = document.querySelector('.temporary');
         // temporary.insertAdjacentText('afterbegin', date1);
