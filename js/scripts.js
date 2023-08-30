a7f5e6335ef2349673f5d434414a2beb0e46b716
Yakov <106259638+YakovSalman@users.noreply.github.com>
2023-08-30 11:09:10 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -590,11 +590,7 @@ function choiceDays () {
       const itemFirst = itemActiveArray.shift();
       const itemLast = itemActiveArray.shift();
 
-      function selectedDate (variableName) {
-        const date1 = new Date
-        const temporary = document.querySelector('.temporary')
-        temporary.insertAdjacentText('afterbegin', date1)
-	      
+      function selectedDate (variableName) {	      
         const currentWrap = variableName.closest('.calendar-wrap');
         const currentMonth = currentWrap.querySelector('.calendar-month');
         const currentYears = currentWrap.querySelector('.calendar-year');
@@ -681,7 +677,9 @@ if(calendar != null) {
     if (e.target.classList.contains('calendar-days-item')){
       e.target.classList.toggle('active');
       const activeEl = calendar.querySelectorAll('.active');
-
+      const date1 = new Date;
+      const temporary = document.querySelector('.temporary')
+      temporary.insertAdjacentText('afterbegin', date1)    
       if (activeEl.length > 2) {
         e.target.classList.remove('active');
       }
